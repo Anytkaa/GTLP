1f96962e8f73a10ed40b1f546fae5d88ad261efa
Karriss <144677707+Karriss@users.noreply.github.com>
2025-03-26 22:24:39 +0300

    Add files via upload

---
+++ GTLP-main/countries/config1.py
@@ -0,0 +1,7 @@
+db_config = {
+    'host': '127.0.0.1',  # Адрес сервера БД
+    'user': 'root',       # Имя пользователя БД
+    'port': '3306',
+    'password': 'Kar_!kar0609', # Пароль
+    'database': 'horoshev36_prj' # Имя вашей БД
+}
