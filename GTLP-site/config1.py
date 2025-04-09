aef46283ba030bd9977148489cf1eb973952b4a2
Anytkaa <144678499+Anytkaa@users.noreply.github.com>
2025-04-09 17:04:30 +0300

    Update config1.py

--- GTLP-site/config1.py
+++ GTLP-site/config1.py
@@ -2,6 +2,6 @@ db_config = {
     'host': '127.0.0.1',  # Адрес сервера БД
     'user': 'root',       # Имя пользователя БД
     'port': '3307',
-    'password': 'Kokos13579*', # Пароль
+    'password': '', # Пароль
     'database': 'Site' # Имя вашей БД
 }
